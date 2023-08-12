da30b5760f6cff3631561bbac3c443fe09737ceb
BrentC0328 <107273004+BrentC0328@users.noreply.github.com>
2023-08-12 15:40:02 -0400

    not doing anything

--- index.ts
+++ index.ts
@@ -5,11 +5,41 @@ import './style.css';
 const form: HTMLFormElement = document.querySelector('#defineform');
 
 
-form.onsubmit = () => {
+form.onsubmit = async () => {
   const formData = new FormData(form);
+  const word = formData.get('defineword') as string;
 
-  console.log(formData);
-  const text = formData.get('defineword') as string;
-  console.log(text);
-  return false; // prevent reload
-};
+  const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+  if(response.ok){
+    const data = await response.json();
+    const theDefinition = "chicken nuggets";
+
+    update(theDefinition); }
+    else { 
+      console.error('API request failed'); 
+    }
+    return false;
+  }
+
+
+
+
+
+
+
+
+const main = document.querySelector('.container') as HTMLDivElement;
+
+function update(word: string){
+
+  const listItems =  '<li>${word}</li>';
+
+  const list = document.createElement('ul');
+  list.className = 'list-unstyled';
+  list.innerHTML = listItems;
+  
+  main.innerHTML = ''; // Clear previous content
+  main.appendChild(list);
+}
+
+
